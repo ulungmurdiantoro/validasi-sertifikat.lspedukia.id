fad233684ac2ef3f2a6fcc207d5315960fe93028
ulung murdiantoro <ulungmurdiantoro@gmail.com>
2024-09-03 11:12:02 +0700

    done

--- resources/js/Pages/Student/Login/Index.vue
+++ resources/js/Pages/Student/Login/Index.vue
@@ -30,9 +30,7 @@
                                         <th class="border-0 rounded-start" style="width:5%">No.</th>
                                         <th class="border-0">Nama Lengkap</th>
                                         <th class="border-0">Skema Sertifikasi</th>
-                                        <!-- <th class="border-0">Nomor Skema</th> -->
                                         <th class="border-0">Nomor Sertifikat</th>
-                                        <!-- <th class="border-0">Nama Gelar</th> -->
                                         <th class="border-0">Certification Date</th>
                                         <th class="border-0 rounded-end">Status Sertifikat</th>
                                     </tr>
@@ -44,9 +42,7 @@
                                         <td class="fw-bold text-center">{{ ++index + (penerimasertifs.current_page - 1) * penerimasertifs.per_page }}</td>
                                         <td>{{ penerimasertif.nama_lengkap }}</td>
                                         <td>{{ penerimasertif.skema }}</td>
-                                        <!-- <td>{{ penerimasertif.no_skema }}</td> -->
                                         <td>{{ penerimasertif.no_sertif }}</td>
-                                        <!-- <td>{{ penerimasertif.nama_gelar }}</td> -->
                                         <td>{{ penerimasertif.tgl_rilis }}</td>
                                         <td v-if="penerimasertif.tgl_rilis < penerimasertif.tgl_berakhir">Aktif</td>
                                         <td v-else>Expired</td>
