96b570c43af0e82da68f7d1fa914d1aeaebb4d31
ulung murdiantoro <ulungmurdiantoro@gmail.com>
2024-09-03 14:45:42 +0700

    done

--- resources/js/Layouts/Student.vue
+++ resources/js/Layouts/Student.vue
@@ -7,32 +7,32 @@
             <div class="collapse navbar-collapse" id="navbarCollapse">
                 <ul class="navbar-nav me-auto mb-2 mb-md-0">
                 </ul>
-                    <a class="text-white" href="https://lspedukia.id/">EDUKIA</a>
+                    <a class="text-white-bold" href="https://lspedukia.id/">EDUKIA</a>
             </div>
             <div class="collapse navbar-collapse" id="navbarCollapse">
                 <ul class="navbar-nav me-auto mb-2 mb-md-0">
                 </ul>
-                    <a class="text-white" href="https://lspedukia.id/">TENTANG KAMI</a>
+                    <a class="text-white-bold" href="https://lspedukia.id/">TENTANG KAMI</a>
             </div>
             <div class="collapse navbar-collapse" id="navbarCollapse">
                 <ul class="navbar-nav me-auto mb-2 mb-md-0">
                 </ul>
-                    <a class="text-white" href="https://lspedukia.id/">SERTIFIKASI</a>
+                    <a class="text-white-bold" href="https://lspedukia.id/">SERTIFIKASI</a>
             </div>
             <div class="collapse navbar-collapse" id="navbarCollapse">
                 <ul class="navbar-nav me-auto mb-2 mb-md-0">
                 </ul>
-                    <a class="text-secondary" href="/">VALIDASI SERTIFIKAT</a>
+                    <a class="text-secondary-bold" href="/">VALIDASI SERTIFIKAT</a>
             </div>
             <div class="collapse navbar-collapse" id="navbarCollapse">
                 <ul class="navbar-nav me-auto mb-2 mb-md-0">
                 </ul>
-                    <a class="text-white" href="https://lspedukia.id/">BERITA</a>
+                    <a class="text-white-bold" href="https://lspedukia.id/">BERITA</a>
             </div>
             <div class="collapse navbar-collapse" id="navbarCollapse">
                 <ul class="navbar-nav me-auto mb-2 mb-md-0">
                 </ul>
-                    <a class="text-white" href="https://lspedukia.id/">KONTAK KAMI</a>
+                    <a class="text-white-bold" href="https://lspedukia.id/">KONTAK KAMI</a>
             </div>
         </div>
     </nav>
